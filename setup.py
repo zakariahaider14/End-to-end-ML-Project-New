5e89f917306386b9efd98868f7672135317c29c0
zakariahaider14 <h.zakaria.business@gmail.com>
2025-05-05 00:11:03 -0400

    trying new files

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project-Implementation"
-AUTHOR_USER_NAME = "entbappy"
-SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+REPO_NAME = "End-to-end-ML-Project-1"
+AUTHOR_USER_NAME = "zakariahaider14"
+SRC_REPO = "mlProject-1"
+AUTHOR_EMAIL = "h.zakaria.business@gmail.com"
 
 
 setuptools.setup(
@@ -19,11 +19,18 @@ setuptools.setup(
     author_email=AUTHOR_EMAIL,
     description="A small python package for ml app",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
